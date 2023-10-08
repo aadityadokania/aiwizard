05653e6c06a1b916f2800b4c19ee7885665f9a16
Aaditya <aadityadokania1@gmail.com>
2023-10-09 05:23:15 +0530

    prisma bug fix

--- components/mobile-sidebar.tsx
+++ components/mobile-sidebar.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import React, { useEffect, useState } from "react";
-import { Button } from "@/components//ui/button";
 import { Menu } from "lucide-react";
 import { Sheet, SheetContent, SheetTrigger } from "./ui/sheet";
 import Sidebar from "./sidebar";
